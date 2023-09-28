29dea2e4b288f96d24dcf8c241a2a5d06f711d1b
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2023-09-28 11:34:50 +0700

    update source

---
+++ springboot-sort/src/main/java/com/server/springbootsort/SpringbootSortApplication.java
@@ -0,0 +1,13 @@
+package com.server.springbootsort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSortApplication.class, args);
+	}
+
+}
