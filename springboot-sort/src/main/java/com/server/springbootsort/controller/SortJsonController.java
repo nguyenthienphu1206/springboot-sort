f736219182ee24ca60254aaa3255422b0c495518
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2023-09-28 11:47:34 +0700

    test

--- springboot-sort/src/main/java/com/server/springbootsort/controller/SortJsonController.java
+++ springboot-sort/src/main/java/com/server/springbootsort/controller/SortJsonController.java
@@ -1,9 +1,8 @@
 package com.server.springbootsort.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,6 +10,12 @@ import java.util.TreeMap;
 @RestController
 @RequestMapping("/api/v1")
 public class SortJsonController {
+
+    @GetMapping
+    public ResponseEntity<String> sendMessage() {
+
+        return new ResponseEntity<>("Successfully", HttpStatus.OK);
+    }
 
     @PostMapping("/sort")
     public Map<String, Object> processJson(@RequestBody Map<String, Object> jsonList) {
